3d00916480df111c301d08a13ae23ccab9ad74d3
Alaa <alaaewais208@gmail.com>
2026-02-03 05:53:28 +0200

    create database

---
+++ models/AppUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace JWT.models
+{
+    public class AppUser:IdentityUser
+    {
+
+
+    }
+}
---
+++ models/UserIdentityContext.cs
@@ -0,0 +1,13 @@
+﻿namespace JWT.models;
+
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+    public class UserIdentityContext : IdentityDbContext<AppUser>
+
+{
+    public UserIdentityContext(DbContextOptions<UserIdentityContext> options) : base(options)
+    { }
+
+}
+
